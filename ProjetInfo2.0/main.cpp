916366a71a4e67b454f4d80b2987bde467fcde88
clement cauffet <clement.cauffet@edu.ece.fr>
2018-04-04 14:45:49 +0200

    Chargement + Sauvegarde

---
+++ ProjetInfo2.0/main.cpp
@@ -0,0 +1,43 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+#define SCREEN_W 1024
+#define SCREEN_H 768
+
+int main()
+{
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+
+    /// Un exemple de graphe
+    Graph g;
+    //g.make_example();
+    g.loadFile("load1");
+
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update();
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+
+        if(key[KEY_S]) g.saveFile("save1.txt");
+
+    }
+
+    grman::fermer_allegro();
+
+    return 0;
+}
+END_OF_MAIN();
+
+
